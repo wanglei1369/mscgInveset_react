872c1ccc002aeafa085234851fad73cad1d4f054
lei wang <wanglei1369@hotmail.com>
2020-09-02 14:16:13 +1200

    changed logofooter

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@
 import React, {Suspense} from "react"
 import {Link} from "react-router-dom"
 import {useTranslation} from "react-i18next"
-import footerImg from "../images/logoFooter.png"
+import footerImg from "../images/logoNavDark.png"
 import "../fonts/iconfont/iconfont.css"
 
 export default function Footer() {
